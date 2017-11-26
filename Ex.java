a88c1d94d5e92d804d0c3874c34590b094f078b0
sunilwipro <sunilkumarac.htl@gmail.com>
2017-11-27 01:40:59 +0530

    2 java files added repository

---
+++ Ex.java
@@ -0,0 +1,13 @@
+
+public class Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;	
+		for(i=1;i<=10;i++)
+			{
+				System.out.println("number is "+i);
+			}
+	}
+
+}
