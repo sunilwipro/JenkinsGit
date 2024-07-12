6d953d21523f02a9c547d8382fa0ae3d5714c396
sunilwipro <33525580+sunilwipro@users.noreply.github.com>
2024-07-12 10:43:27 +0000

    modified Ex.java

--- Ex.java
+++ Ex.java
@@ -3,7 +3,7 @@ public class Ex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the Poll SCM example")
+		System.out.println("This is the Poll SCM example");
 		/*int i;	
 		for(i=1;i<=10;i++)
 			{
