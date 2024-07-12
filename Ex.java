89a871c4678f2dee56b60db65ac8087f78500b25
sunilwipro <33525580+sunilwipro@users.noreply.github.com>
2024-07-12 10:46:14 +0000

    Update Ex.java

--- Ex.java
+++ Ex.java
@@ -4,12 +4,12 @@ public class Ex {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the Poll SCM example");
-		/*int i;	
+		int i;	
 		for(i=1;i<=10;i++)
 			{
 				System.out.println("number is "+i);
 			}
-		*/
+		
 	}
 
 }
