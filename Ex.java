e698ce99248626de0c45d6ebc0884b33cacbb9be
sunilwipro <33525580+sunilwipro@users.noreply.github.com>
2024-07-12 10:36:15 +0000

    print statement is added on Ex.java

--- Ex.java
+++ Ex.java
@@ -3,11 +3,13 @@ public class Ex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i;	
+		System.out.println("This is the Poll SCM example")
+		/*int i;	
 		for(i=1;i<=10;i++)
 			{
 				System.out.println("number is "+i);
 			}
+		*/
 	}
 
 }
